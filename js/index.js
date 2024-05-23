f64082205b68b18e20996d8aa08e6bca18067b33
jMarczak44 <252813@edu.p.lodz.pl>
2024-05-23 15:06:24 +0200

    nowa wersja /problem z css i zdj w starej stronie

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-document.getElementById('credValid').addEventListener('submit', function(event) 
-{
+document.getElementById('credValid').addEventListener('submit', function (event) {
     //stopper
     event.preventDefault();
 
@@ -16,36 +15,32 @@ document.getElementById('credValid').addEventListener('submit', function(event)
     document.getElementById('emailError').innerText = '';
 
     // imie
-    if (firstName === '') 
-        {
-            document.getElementById('firstNameError').innerText = 'Pole imię nie może być puste!';
-            isValid = false;
-        }
+    if (firstName === '') {
+        document.getElementById('firstNameError').innerText = 'Pole imię nie może być puste!';
+        isValid = false;
+    }
 
     // naziwsko
-    if (lastName === '') 
-        {
-            document.getElementById('lastNameError').innerText = 'Pole nazwisko nie może być puste!';
-            isValid = false;
-        }
+    if (lastName === '') {
+        document.getElementById('lastNameError').innerText = 'Pole nazwisko nie może być puste!';
+        isValid = false;
+    }
 
     // email
     const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (email === '') 
-        {
-            document.getElementById('emailError').innerText = 'Pole email nie może być puste!';
-        
-            isValid = false;
-    
-        } else if (!emailValid.test(email)) 
-            {
-                document.getElementById('emailError').innerText = 'Podany email nie jest poprawny!';
-        
-                isValid = false;
-            }
+    if (email === '') {
+        document.getElementById('emailError').innerText = 'Pole email nie może być puste!';
+
+        isValid = false;
+
+    } else if (!emailValid.test(email)) {
+        document.getElementById('emailError').innerText = 'Podany email nie jest poprawny!';
+
+        isValid = false;
+    }
 
     if (isValid) {
         alert('Dane zatwierdzone!');
-       
+
     }
 });
