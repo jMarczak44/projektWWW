54843596b6a3aa366e96fd446689617366209971
vapordiver <dawidkoska217@gmail.com>
2024-05-23 13:53:45 +0200

    skrypt od nowa

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-const form = document.getElementById('form');
-const fname = document.getElementById('fname');
-const surname = document.getElementById('surname');
-const email = document.getElementById('email');
-
-form.addEventListener('submit', x => 
-    {
-        x.preventDefault();
-
-        validateInput();
-    });
-
-const validateInput = () =>
-    {
-        const tempFname = fname.value.trim();
-        const tempSurname = surname.value.trim();
-        const tempEmail = email.value.trim();
+document.getElementById('myForm').addEventListener('submit', function(event) 
+{
+    //stopper
+    event.preventDefault();
     
-    };
+    var isValid = true;
+
+    // pobieranie wartosci z forma
+    const firstName = document.getElementById('firstName').value.trim();
+    const lastName = document.getElementById('lastName').value.trim();
+    const email = document.getElementById('email').value.trim();
+
+    // reset error
+    document.getElementById('firstNameError').innerText = '';
+    document.getElementById('lastNameError').innerText = '';
+    document.getElementById('emailError').innerText = '';
+
+    // imie
+    if (firstName === '') 
+        {
+            document.getElementById('firstNameError').innerText = 'Pole imię nie może być puste!';
+            isValid = false;
+        }
+
+    // naziwsko
+    if (lastName === '') 
+        {
+            document.getElementById('lastNameError').innerText = 'Pole nazwisko nie może być puste!';
+            isValid = false;
+        }
+
+    // email
+    const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email === '') 
+        {
+            document.getElementById('emailError').innerText = 'Pole email nie może być puste!';
+        
+            isValid = false;
+    
+        } else if (!emailValid.test(email)) 
+            {
+                document.getElementById('emailError').innerText = 'Podany email nie jest poprawny!';
+        
+                isValid = false;
+            }
+
+    if (isValid) {
+        alert('Dane zatwierdzone!');
+       
+    }
+});
